ce767cbe1b9e071d22860db6dfcc7c0f9fc4eb7f
jaubourg <j@ubourg.net>
2012-09-23 02:34:30 +0200

    OH HAI IT5 2012!

--- build/data/intro.js
+++ build/data/intro.js
@@ -1,7 +1,7 @@
 /*!
  * jQuery ajaxHooks v@VERSION
  *
- * Copyright 2011, Julian Aubourg
+ * Copyright 2012, Julian Aubourg
  * Dual licensed under the MIT or GPL Version 2 licenses.
  *
  * Date: @DATE
