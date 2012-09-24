fd499fd9a05b599e5af60a158c837cff825e538e
jaubourg <j@ubourg.net>
2012-09-24 15:39:22 +0200

    Changes license to MIT only

--- build/intro.js
+++ build/intro.js
@@ -2,7 +2,7 @@
  * jQuery ajaxHooks v@VERSION
  *
  * Copyright 2012, Julian Aubourg
- * Dual licensed under the MIT or GPL Version 2 licenses.
+ * licensed under the MIT license
  *
  * Date: @DATE
  */
