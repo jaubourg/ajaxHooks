45e51b393e218461fb2c85b69b30f5629726f9a3
jaubourg <j@ubourg.net>
2012-09-29 03:23:30 +0200

    handles complete and properly fires each callback once and only once

--- src/ajax/retry.js
+++ src/ajax/retry.js
@@ -2,8 +2,13 @@ jQuery.ajaxPrefilter(function( options, originalOptions, jqXHR ) {
 	if ( options.retry ) {
 		var url = options.url,
 			oldPromise = jqXHR.promise();
+		if ( options._retried ) {
+			options.success = options.error = options.complete = undefined;
+		} else {
+			originalOptions = jQuery.extend( {}, originalOptions, { _retried: true } );
+		}
 		jqXHR.pipe( null, function() {
-			if ( options.retry( originalOptions, jqXHR ) ) {
+			if ( options.retry.call( options.context || options, originalOptions, jqXHR ) ) {
 				return jQuery.ajax( originalOptions.url || url, originalOptions );
 			}
 			return oldPromise;
@@ -11,6 +16,12 @@ jQuery.ajaxPrefilter(function( options, originalOptions, jqXHR ) {
 		if ( jqXHR.success ) {
 			jqXHR.success = jqXHR.done;
 			jqXHR.error = jqXHR.fail;
+			jqXHR.complete = (function( callbacks ) {
+				jqXHR.always(function( error, statusText, success ) {
+					callbacks.fireWith( this, [ jqXHR.state() === "resolved" ? success : error ] );
+				});
+				return callbacks.add;
+			})( jQuery.Callbacks( "once memory" ) );
 		}
 	}
 });
--- test/unit/ajax/retry.js
+++ test/unit/ajax/retry.js
@@ -1,70 +1,118 @@
 module( "ajax/retry", { teardown: moduleTeardown } );
 
 test( "basic counter", function() {
-	expect( 1 );
+	expect( 12 );
 	stop();
-	var count = 0,
+	var context = {},
+		count = 0,
 		retries = 0;
 	jQuery.ajax( "WILL_404", {
+		context: context,
 		beforeSend: function() {
 			count++;
 		},
 		retry: function( options, jqXHR ) {
+			strictEqual( this, context, "context ok (retry)" );
 			return ( jqXHR.status === 404 ) && ( ( retries++ ) < 3 );
+		},
+		error: function() {
+			strictEqual( this, context, "context ok (error)" );
+			strictEqual( count, 4, "we had 3 retries (error)" );
+		},
+		complete: function() {
+			strictEqual( this, context, "context ok (internal complete)" );
+			strictEqual( count, 4, "we had 3 retries (internal complete)" );
 		}
 	}).done(function() {
 		strictEqual( arguments, [] );
 	}).fail(function() {
-		strictEqual( count, 4, "we had 3 reties" );
+		strictEqual( this, context, "context ok (fail)" );
+		strictEqual( count, 4, "we had 3 retries (fail)" );
+	}).complete(function() {
+		strictEqual( this, context, "context ok (external complete)" );
+		strictEqual( count, 4, "we had 3 retries (external complete)" );
 	}).always(function() {
 		start();
 	});
 });
 
 test( "test urls (success)", function() {
-	expect( 1 );
+	expect( 9 );
 	stop();
-	var URLs = [ "WILL_404_1", "WILL_404_2", "WILL_404_3", "data/proper.json" ];
+	var context = {},
+		URLs = [ "WILL_404_1", "WILL_404_2", "WILL_404_3", "data/proper.json" ];
 	jQuery.ajax( URLs.shift(), {
+		context: context,
 		dataType: "json",
 		retry: function( options, jqXHR ) {
 			var newURL = jqXHR.status === 404 && URLs.shift();
+			strictEqual( this, context, "context ok (retry)" );
 			if ( newURL ) {
 				options.url = newURL;
 				return true;
 			}
+		},
+		success: function( data ) {
+			strictEqual( this, context, "context ok (success)" );
+			deepEqual( data, {
+				"hello": 78,
+				"world": "string"
+			}, "proper data received (success)" );
+		},
+		complete: function() {
+			strictEqual( this, context, "context ok (internal complete)" );
 		}
 	}).done(function( data ) {
+		strictEqual( this, context, "context ok (done)" );
 		deepEqual( data, {
 			"hello": 78,
 			"world": "string"
-		}, "proper data received" );
+		}, "proper data received (done)" );
 	}).fail(function() {
 		strictEqual( arguments, [] );
+	}).complete(function() {
+		strictEqual( this, context, "context ok (external complete)" );
 	}).always(function() {
 		start();
 	});
 });
 
 test( "test urls (fail)", function() {
-	expect( 3 );
+	expect( 15 );
 	stop();
-	var URLs = [ "WILL_404_1", "WILL_404_2", "WILL_404_3" ];
+	var URLs = [ "WILL_404_1", "WILL_404_2", "WILL_404_3" ],
+		context = {};
 	jQuery.ajax( URLs.shift(), {
+		context: context,
 		dataType: "json",
 		retry: function( options, jqXHR ) {
+			strictEqual( this, context, "context ok (retry)" );
 			var newURL = jqXHR.status === 404 && URLs.shift();
 			if ( newURL ) {
 				options.url = newURL;
 				return true;
 			}
+		},
+		error: function( jqXHR, type, error ) {
+			strictEqual( this, context, "context ok (error)" );
+			strictEqual( jqXHR.status, 404, "404 detected (error)" );
+			strictEqual( type, "error", "error type OK (error)" );
+			strictEqual( error, "Not Found", "status text OK (error)" );
+		},
+		complete: function( jqXHR ) {
+			strictEqual( this, context, "context ok (internal complete)" );
+			strictEqual( jqXHR.status, 404, "404 detected (internal complete)" );
 		}
 	}).done(function( data ) {
 		strictEqual( arguments, [] );
 	}).fail(function( jqXHR, type, error ) {
-		strictEqual( jqXHR.status, 404, "404 detected" );
-		strictEqual( type, "error", "error type OK" );
-		strictEqual( error, "Not Found", "status text OK" );
+		strictEqual( this, context, "context ok (fail)" );
+		strictEqual( jqXHR.status, 404, "404 detected (fail)" );
+		strictEqual( type, "error", "error type OK (fail)" );
+		strictEqual( error, "Not Found", "status text OK (fail)" );
+	}).complete(function( jqXHR ) {
+		strictEqual( this, context, "context ok (external complete)" );
+		strictEqual( jqXHR.status, 404, "404 detected (external complete)" );
 	}).always(function() {
 		start();
 	});
